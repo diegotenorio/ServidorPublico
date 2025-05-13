8ce03df1db07506ada5145d9942f67c3229ff1d4
diegotenorio <dtg777@gmail.com>
2025-05-13 03:43:24 +0000

    criando docker

--- ServidorPublico.API/Program.cs
+++ ServidorPublico.API/Program.cs
@@ -1,46 +1,3 @@
-/*using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using System.Reflection;
-using MediatR;
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using ServidorPublico.Application.Validators;
-using ServidorPublico.Infrastructure;
-using ServidorPublico.API.Middlewares;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddValidatorsFromAssemblyContaining<CreateServidorValidator>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Injeção de dependência
-builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseInMemoryDatabase("ServidorDb"));
-
-builder.Services.AddMediatR(Assembly.Load("ServidorPublico.Application"));
-
-builder.Services.AddValidatorsFromAssemblyContaining<CreateServidorValidator>();
-
-var app = builder.Build();
-
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
-
-// Middleware global de erros
-app.UseMiddleware<ExceptionMiddleware>();
-
-app.UseAuthorization();
-app.MapControllers();
-app.Run();*/
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
@@ -61,10 +18,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("ServidorDb"));
 builder.Services.AddMediatR(Assembly.Load("ServidorPublico.Application"));
 builder.Services.AddValidatorsFromAssemblyContaining<CreateServidorValidator>();
-
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-
-
 
 var app = builder.Build();
 
